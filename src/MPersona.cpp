bed2796bdb8689733b41bc6b6cf396903e07b322
Dorieliz Guerrero <dorielizguerrero2000@gmail.com>
2023-01-30 20:11:35 -0400

    Configuraciones para proyecto Eclipse

--- src/MPersona.cpp
+++ src/MPersona.cpp
@@ -15,57 +15,45 @@ MPersona::~MPersona() {
 
 }
 
-bool MPersona::IncluirRol(InfoPersona persona){
+bool MPersona::IncluirRol(InfoPersona persona) {
 	return Insertar(persona);
 
 }
-Nodo<InfoPersona>* MPersona::BuscarPersona(string cedulaPersona){
+
+Nodo<InfoPersona>* MPersona::BuscarPersona(string cedulaPersona) {
 	Nodo<InfoPersona> *p = frente->ObtDer();
-	int marca,valor;
+	int marca, valor;
 	marca = -1;
 	bool encontro;
-	if (!Vacia()){
+	if (!Vacia()) {
 		Insertar(marca);
-	}	encontro = false;
-		while (!(encontro)) {
-			if (p->ObtInfo().cedula == cedulaPersona) {
-				break;
-			}
-			Remover(valor);
-			if (valor == marca)
-				encontro = true;
-			else {
-
-			}
+	}
+	encontro = false;
+	while (!(encontro)) {
+		if (p->ObtInfo().cedula == cedulaPersona) {
+			break;
 		}
-		return p;
+		Remover(valor);
+		if (valor == marca)
+			encontro = true;
+		else {
+
+		}
+	}
+	return p;
 
 }
-bool MPersona::ModificarPersona(InfoPersona persona){
+
+bool MPersona::ModificarPersona(InfoPersona persona) {
 	Nodo<InfoPersona> *p = this->BuscarPersona(persona.cedula);
-		if (p != NULL) {
-			p->AsigInfo(persona);
-			return true;
-		}
-		return false;
+	if (p != NULL) {
+		p->AsigInfo(persona);
+		return true;
+	}
+	return false;
 
 }
-bool MPersona::EliminarPersona (string cedulaPersona){
-	Nodo<InfoPersona> *p = this->BuscarPersona(persona.cedula);
 
-	Nodo<InfoPersona> *primero;
-	    if (p != NULL)
-	    {
-	        primero = frente;
-	        persona = primero->ObtInfo();
-	        frente = primero->ObtDer();
-	        if (frente == NULL)
-	            final = NULL;
-	        delete primero;
-	        return true;
-	    }
-	    else
-	        return false;
-
-
+bool MPersona::EliminarPersona(string cedulaPersona) {
+	return true;
 }
